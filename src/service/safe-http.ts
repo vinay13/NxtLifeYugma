185afd7505c6d5d1868b0bc73a02c9028a00aaf7
TusharBalar <balar.tushar1@gmail.com>
2016-11-23 15:13:06 +0530

    Improve safe-http.ts (Exception handling + headers)

--- src/service/safe-http.ts
+++ src/service/safe-http.ts
@@ -1,20 +1,24 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { RequestOptions } from '@angular/http';
+import { RequestOptions, Headers } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
+import { Configuration } from './app.constants';
 import { LoadingController, AlertController, ToastController } from 'ionic-angular';
 
 @Injectable()
 export class SafeHttp {
 
+  private access_token;
+  private headers;
+
   constructor(private http: Http,
               private alertCtrl: AlertController,
               private toastCtrl: ToastController,
+              private _configuration: Configuration,
               private loadingCtrl: LoadingController) { }
 
-  
   public ErrorMessage() {
     let toast = this.toastCtrl.create({
       message: 'Internal server error',
@@ -27,29 +31,53 @@ export class SafeHttp {
   get(url: string, options?: RequestOptions): Promise<any> {
 
     let loader = this.loadingCtrl.create({
-      content: "Loading..."
+      content: "Fetching Data..."
     });
-    
+
     loader.present();
+
+    this.access_token = this._configuration.getAccessToken();
+
+    this.headers = new Headers({
+      'Content-Type' : 'application/json',
+      'Authorization' : 'Bearer ' + this.access_token
+    });
+
+    var options = new RequestOptions({
+      headers : this.headers
+    });
 
     return this.http.get(url, options)
       .toPromise()
       .then(response => {
+        console.log("SafeHttp", response)
         loader.dismiss();
         return Promise.resolve(response.json());
       }, function(err) {
+        console.log('err1', err)
         loader.dismiss();
         return Promise.reject(err || 'Server error');
       });
   }
 
   post(url: string, body: string, options?: RequestOptions) {
-  
+
     let loader = this.loadingCtrl.create({
-      content: "Loading..."
+      content: "Fetching Data..."
     });
-    
+
     loader.present();
+
+    this.access_token = this._configuration.getAccessToken();
+
+    this.headers = new Headers({
+      'Content-Type' : 'application/json',
+      'Authorization' : 'Bearer ' + this.access_token
+    });
+
+    var options = new RequestOptions({
+      headers : this.headers
+    });
 
     return this.http.post(url, body, options)
       .toPromise()
