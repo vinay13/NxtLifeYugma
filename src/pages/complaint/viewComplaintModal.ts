cacde8e8bce951e893d3efc08ad2b72d905a9858
TusharBalar <balar.tushar1@gmail.com>
2016-11-27 23:15:04 +0530

    Pass data from complaints to view complaint

--- src/pages/complaint/viewComplaintModal.ts
+++ src/pages/complaint/viewComplaintModal.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 
+import { ViewController } from 'ionic-angular';
+
 @Component({
   selector: 'view-complaint-modal',
   templateUrl: 'viewComplaintModal.html'
@@ -10,13 +12,18 @@ export class viewComplaintModal {
 
   complaint;
 
-  constructor(private navParams: NavParams) {
+  constructor(private navParams: NavParams,
+              private viewCtrl: ViewController) {
 
   }
 
   ngOnInit() {
     this.complaint = this.navParams.get('complaint');
     console.log("complaint", this.complaint);
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
