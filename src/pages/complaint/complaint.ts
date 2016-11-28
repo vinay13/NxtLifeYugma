f636b97b808cb811da4dc875505329bd560e4dbb
TusharBalar <balar.tushar1@gmail.com>
2016-11-28 17:44:46 +0530

    Minor changes in complaint.ts

--- src/pages/complaint/complaint.ts
+++ src/pages/complaint/complaint.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from 'ionic-angular';
+import { ModalController, AlertController, ActionSheetController, ItemSliding } from 'ionic-angular';
 
 import { newComplaintModal } from './newComplaintModal';
 import { viewComplaintModal } from './viewComplaintModal';
 
 import { ComplaintService } from '../../service/complaint.service';
+
+import * as _ from 'underscore';
 
 @Component({
   selector: 'page-speaker-list',
@@ -16,6 +18,8 @@ export class ComplaintPage implements OnInit {
   complaints;
 
   constructor(public modalCtrl: ModalController,
+              private alertCtrl: AlertController,
+              private actionSheetCtrl: ActionSheetController,
               private complaintService: ComplaintService) {
 
   }
@@ -34,6 +38,57 @@ export class ComplaintPage implements OnInit {
   viewComplaint(complaint): void {
     let viewComplaint = this.modalCtrl.create(viewComplaintModal, {complaint: complaint});
     viewComplaint.present();
+  }
+
+  showPrompt(slidingItem: ItemSliding) {
+    let prompt = this.alertCtrl.create({
+      title: 'Why you want to close this complaint?',
+      message: "",
+      inputs: [
+        {
+          name: 'Why?',
+          placeholder: 'Write short description'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            slidingItem.close();
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            slidingItem.close();
+            this.presentActionSheet();
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Close Complaint ?',
+      buttons: [
+        {
+          text: 'Submit',
+          icon: 'ios-paper-outline',
+          handler: () => {
+          }
+        },{
+          text: 'Cancel',
+          icon: 'md-close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
 }
