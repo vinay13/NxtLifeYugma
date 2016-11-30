2a1f4749473be5acb1c49cc51b4fc0e29817d01e
Pankaj <kharetwal.pankaj111@gmail.com>
2016-11-30 17:16:27 +0530

    Linked click functions to components

--- src/pages/homepage/homepage.ts
+++ src/pages/homepage/homepage.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { PollPage } from '../poll/poll';
+import { SuggestionPage } from '../suggestion/suggestion';
+import { AppreciationPage } from '../appreciation/appreciation';
+import { ReportIssuePage} from '../reportIssue/reportIssue';
+import { SurveyPage} from '../survey/survey';
+import { ComplaintPage} from '../complaint/complaint';
 
 @Component({
   selector: 'page-dashboard',
@@ -7,7 +15,31 @@ import { Component } from '@angular/core';
 
 export class Dashboard {
 
-  constructor() {
+  constructor(private navCtrl: NavController) {
+  }
+  
+  openComplaint() {
+    this.navCtrl.setRoot(ComplaintPage);
+  }
+
+  openPoll() {
+    this.navCtrl.setRoot(PollPage);
+  }
+
+  openSuggestion() {
+    this.navCtrl.setRoot(SuggestionPage);
+  }
+
+  openAppreciation() {
+    this.navCtrl.setRoot(AppreciationPage);
+  }
+
+  openSurvey() {
+    this.navCtrl.setRoot(SurveyPage);
+  }
+
+  openReportIssue() {
+    this.navCtrl.setRoot(ReportIssuePage);
   }
 
 }
