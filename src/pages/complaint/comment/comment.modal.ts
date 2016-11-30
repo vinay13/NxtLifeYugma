f76bd6d660c619d4c715334007bd73dc9272d0a8
TusharBalar <balar.tushar1@gmail.com>
2016-11-30 13:23:00 +0530

    Impliment form group in comment

--- src/pages/complaint/comment/comment.modal.ts
+++ src/pages/complaint/comment/comment.modal.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ViewController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'comment-modal',
   templateUrl: 'comment.modal.html'
 })
 
-export class CommentModal {
+export class CommentModal  implements OnInit {
 
-  constructor(private viewCtrl: ViewController) {
+  ComplaintComment: FormGroup;
+  comment: any;
 
+  constructor(private viewCtrl: ViewController,
+              private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.ComplaintComment = this.formBuilder.group({
+      comment: ['', Validators.compose([Validators.required])]
+    });
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  postComment() {
+    if (!this.ComplaintComment.valid) {
+      console.log("not valid form");
+    } else {
+      console.log("valid form", this.ComplaintComment.value)
+    }
   }
 
 }
